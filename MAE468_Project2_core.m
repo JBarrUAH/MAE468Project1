225f6bc5cd4df799c3123518baba15edc662f3f5
JBarrUAH <jpb0033@uah.edu>
2021-04-17 16:03:11 -0500

    Initial project 2 commit
    Initialized core file for project 2, will create a branch for further editing.

---
+++ MAE468_Project2_core.m
@@ -0,0 +1,22 @@
+% This is the core code file for the MAE468 Project 2 submission
+% The team consists of Joseph Barragree, Sarah Polickoski, Micajah
+% Schweikert, and Stephen Ward.
+%% Notes
+% Notes go here
+
+%% Housekeeping
+% Run to remove figures, workspace variables and command window content
+format compact
+close all
+clear
+clc
+
+%% Variable Initialization
+
+
+
+
+
+%% Functions
+
+
