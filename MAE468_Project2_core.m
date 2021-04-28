ae75ed14b90f6ca2d5af66f025f6bf59a986ba13
JBarrUAH <jpb0033@uah.edu>
2021-04-28 10:20:31 -0500

    Updated ADCS component parameters
    Selected smaller reaction wheels and revised momentum dump. We could possibly get away without the small thrusters (over mission life the wheels see only 18% saturation), but it is likely better to keep them from a redundancy standpoint.

--- MAE468_Project2_core.m
+++ MAE468_Project2_core.m
@@ -5,7 +5,7 @@
 %% Notes
 %source used for some constants: http://www.dept.aoe.vt.edu/~lutze/AOE2104/consts.pdf
 %source for Mars and Earth constants https://nssdc.gsfc.nasa.gov/planetary/factsheet/marsfact.html
-%ADCS reaction wheel source, using HR 14-50 https://satcatalog.com/datasheet/Honeywell%20-%20HR12-25.pdf
+%ADCS reaction wheel source, using HR 12-25 https://satcatalog.com/datasheet/Honeywell%20-%20HR12-25.pdf
 %ADCS thruster source, using MR-103J https://www.rocket.com/sites/default/files/documents/In-Space%20Data%20Sheets%204.8.20.pdf
 %Launch Vehicle source, using Falcon Heavy https://www.spacelaunchreport.com/falconH.html
 %Launch Vehicle cost, Falcon Heavy https://www.faa.gov/about/office_org/headquarters_offices/ast/media/2018_ast_compendium.pdf
@@ -135,9 +135,9 @@ SCmI(4)=2/5*SCmI(1)*SCdim(1)^2+((SolarArray(1)/4+0.25)^2+((SolarArray(1)/2)^2+2^
 
 SCdstrb=Tgrav(SCmI(4),SCmI(3),Mdist(2)+alt,muM,5);%symmetric solar panels, so only grav-gradient torque for 5deg attitude
 SCdstrb(2)=SCdstrb(1)*(TlM+TnM)/4*0.707; %orbit total disturbance in N*m*s
-[ADCS(1),ADCS(2)]=Mdump(50*SCdstrb(2),SCdim(1),30,202); %dumping stored momentum, some manual iteration required to find parameters to keep required thrust under 1N
-ADCS(3)=4*(8.5)+8*(0.37)+(ADCS(2)*((60*60*24*365.2425*17)/(TlM+TnM))/50); %ADCS mass, accounts for 4 reaction wheels, 8 thrusters, and all necessary fuel
-fprintf("ADCS System Parameters\n\t Stored momentum for 50 orbits: %5.2f N*m*s\n\t Thrust for 30 second momentum dump: %4.2f N\n\t Fuel required for mission life: %4.2f kg\n\t ADCS system mass: %5.2f kg\n",50*SCdstrb(2),ADCS(1),ADCS(2)*((60*60*24*365.2425*17)/(TlM+TnM))/50,ADCS(3));
+[ADCS(1),ADCS(2)]=Mdump(500*SCdstrb(2),SCdim(1),2,202); %dumping stored momentum, some manual iteration required to find parameters to keep required thrust under 1N
+ADCS(3)=4*(6)+8*(0.37)+(ADCS(2)/500*((60*60*24*365.2425*17)/(TlM+TnM))); %ADCS mass, accounts for 4 reaction wheels, 8 thrusters, and all necessary fuel
+fprintf("ADCS System Parameters\n\t Stored momentum for 500 orbits: %5.2f N*m*s\n\t Thrust for 2 second momentum dump: %4.2f N\n\t Fuel required for mission life: %4.2f kg\n\t ADCS system mass: %5.2f kg\n",500*SCdstrb(2),ADCS(1),ADCS(2)/500*((60*60*24*365.2425*17)/(TlM+TnM)),ADCS(3));
 
 %% Propulsion sizing
 % Select engine parameters for an efficient but powerful enough chemical
